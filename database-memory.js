81ce4cd3bfaa0df6c45162a92a912d9dd1878ddb
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2024-02-14 11:19:29 -0300

    integração de todas as funcionalidades de um cruds, incluindo query params, body params

--- database-memory.js
+++ database-memory.js
@@ -3,8 +3,29 @@ import { randomUUID } from "node:crypto"
 export class DatabaseMemory {
     #courses = new Map()
 
-    list() {
-        return this.#courses.values();
+    getById(id) {
+        return this.#courses.get(id);
+    }
+
+    list(queryTitle) {
+        return Array.from(
+            this.#courses.entries()
+        )
+            .map((courseArray) => {
+            const id = courseArray[0];
+            const data = courseArray[1];
+
+            return {
+                id,
+                ...data
+            }
+        })
+            .filter(course => {
+                if (queryTitle) {
+                    return course.title.includes(queryTitle)
+                }
+                return true
+            })
     }
 
     create(course) {
--- server.js
+++ server.js
@@ -1,22 +1,52 @@
-import { fastify } from "fastify";
-import { DatabaseMemory } from "./database-memory.js";
+import {fastify} from "fastify";
+import {DatabaseMemory} from "./database-memory.js";
 
 const server = fastify();
+const db = new DatabaseMemory();
 
-server.post('/courses', () => {
+server.get('/courses', (req, res) => {
+    const queryTitle = req.query.title;
 
+    return db.list(queryTitle);
+})
+
+server.post('/courses', (req, res) => {
+    const { title, description, duration } = req.body
+
+    const course = {
+        title,
+        description,
+        duration
+    }
+
+    db.create(course)
+
+    return res.status(201).send(course);
 })
 
 server.get('/courses/:id', (req, res) => {
-
+    return db.getById(req.params.id);
 })
 
 server.put('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const { title, description, duration } = req.body
+    const course = {
+        title,
+        description,
+        duration
+    }
 
+    db.update(id, course);
+
+    return res.status(204).send(course)
 })
 
 server.delete('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    db.delete(id);
 
+    return res.status(204).send();
 })
 
 server.listen({ port: 8080 });
